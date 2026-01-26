9cd79053c22d4319df2c1a669902be9d43fa8561
Bilal Usman <bilalusman1291@gmail.com>
2026-01-26 16:28:04 +0500

    updates files

--- Installing Express and Postman/app.js
+++ Installing Express and Postman/app.js
@@ -1,1 +1,15 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+  res.json({ name: "Bilal" });
+});
+
+
+app.listen(3000, "127.0.0.1", () => {
+    console.log(`server in running: http://127.0.0.1:3000/`);
+});
